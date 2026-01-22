e08d2b66e740a5c829a5695e2e50da0dbae14a28
SimbaDuZOO <antoinecombaldieu@gmail.com>
2026-01-22 20:01:00 +0100

    trying something

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,16 +1,18 @@
-
 import { Login } from "@ory/elements-react/theme"
 import { getLoginFlow, OryPageParams } from "@ory/nextjs/app"
-
 import config from "@/ory.config"
+import { cookies } from "next/headers"
 
 export default async function LoginPage(props: OryPageParams) {
+  // Access cookies to ensure they're available in the server context
+  await cookies()
+  
   const flow = await getLoginFlow(config, props.searchParams)
-
+  
   if (!flow) {
     return null
   }
-
+  
   return (
     <Login
       flow={flow}
